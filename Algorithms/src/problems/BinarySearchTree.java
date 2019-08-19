20845fddf01ac50394da79cbab16fcaa26706587
mosdamatac <mos.damatac@gmail.com>
2019-08-18 18:03:38 -0700

    Update BinarySearchTree.java
    - remove comments that has intended step by step process

--- Algorithms/src/problems/BinarySearchTree.java
+++ Algorithms/src/problems/BinarySearchTree.java
@@ -1,38 +1,62 @@
 package problems;
 
+import java.util.*;
+
 import object.BinaryNode;
 
 public class BinarySearchTree {
 
 	public static void main(String[] args) {
-		getDistance(new int[] {5,6,3,1,2,4}, 6, 2, 4);
-
+		BinarySearchTree myClass = new BinarySearchTree();
+		int[] array = {5,6,3,1,2,4};
+		int n = 6;
+		int node1 = 1;
+		int node2 = 2;
+		int distance = myClass.getDistance(array, n, node1, node2);
+		System.out.printf("Distance from %d to %d: %d", node1, node2, distance);
 	}
 	
-	public static int getDistance(int[] array, int n, int node1, int node2) {
+	public int getDistance(int[] array, int n, int node1, int node2) {
 		BinaryNode root = createBinaryTree(array, n);
+		List<Integer> visited1 = traverseTree(root, node1);
+		List<Integer> visited2 = traverseTree(root, node2);
 		
-		// find node1 first
-		// node curr = temporary node
-		// iterate from root and check if node1 < or > root
-		// 
-		return 0;
+		if (visited1.isEmpty() || visited2.isEmpty()) { return -1; }
+		if (node1 == node2) { return 0; }
+		
+		int min = Math.min(visited1.size(), visited2.size());
+		int lcaDistance = 0;
+		while ((lcaDistance+1 < min) && visited1.get(lcaDistance+1) == visited2.get(lcaDistance+1)) {
+			lcaDistance++;
+		}
+		
+		System.out.printf("v1: %s -- v2: %s -- lca: %d\n", visited1, visited2, lcaDistance);
+		return Math.abs((visited1.size() - 1) + (visited2.size() - 1) - (2 * lcaDistance));
+	}
+	
+
+	
+	private List<Integer> traverseTree(BinaryNode root, int node) {		
+		BinaryNode curr = root;
+		List<Integer> visited = new ArrayList<>();
+		
+		while (curr != null) {
+			visited.add(curr.value);
+			if (curr.value == node) {
+				return visited;
+			}
+			
+			if (node < curr.value) {
+				curr = curr.left;
+			} else {
+				curr = curr.right;
+			}
+		}
+		
+		return null;
 	}
 
-	public static BinaryNode createBinaryTree(int[] array, int n) {
-		// create BinaryNode root = new BinaryNode(array[0]);
-		// iterate through array and create a BinaryNode for each value : 1 -> n-1
-			// new node : curr = root
-			// new node : value = e
-			// boolean set = false
-			// while (!set)
-				// e < curr.value
-					// curr.left != null : curr = curr.left
-					// else, curr.left = node e; set = true;
-				// e > curr.right
-					// curr.right != null : curr = curr.right
-					// else, curr.right = node e; set = true;
-		
+	private BinaryNode createBinaryTree(int[] array, int n) {
 		BinaryNode root = new BinaryNode(array[0]);
 		for (int i = 1; i < array.length; i++) {
 			BinaryNode curr = root;
